30c5b0786e82f44064f4dd33ce6a1f094f8aed7e
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-30 18:23:06 +0900

    add

--- main.rs
+++ main.rs
@@ -2,4 +2,9 @@ fn main() {
     // !を使用すると、マクロを呼び出すことができる
     // println!は、マクロ
     println!("Hello, world!");
+    
+    // 型が違う為、エラー
+    // let x: f32 = 2.9;
+    // let y: f64 = 3.0;
+    // println!("{}", x + y);
 }

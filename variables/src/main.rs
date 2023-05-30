7e26ea33f692d72bc8e016d9b877f68e841ef1b9
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-30 17:46:22 +0900

    add shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,23 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    // mut
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;
+    // println!("The value of x is: {}", x);
 
     println!("MAX_POINTS: {}", MAX_POINTS);
+
+    // shadowing
+    let x = 5;
+
+    let x = x + 1; // 6
+
+    {
+        let x = x * 2; // 12
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x); // 6
 }
 
 // 定数はどんなスコープでも定義できる

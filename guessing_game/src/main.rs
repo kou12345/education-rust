837a01c45509282e14daf63547471981cdb49e4a
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-30 16:45:53 +0900

    generate random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,20 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+
+    // 乱数の生成
+    let secret_number: i32 = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
     // mut 可変
     // String型の新しいインスタンスを生成
-    let mut guess = String::new();
+    let mut guess: String = String::new();
 
     io::stdin()
         // &mut guess は guessの参照

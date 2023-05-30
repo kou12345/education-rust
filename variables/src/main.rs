c54a440eddb1f0ef723c618386db45705aa66072
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-30 17:40:16 +0900

    add mut and const

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    println!("MAX_POINTS: {}", MAX_POINTS);
+}
+
+// 定数はどんなスコープでも定義できる
+const MAX_POINTS: u32 = 100_000;

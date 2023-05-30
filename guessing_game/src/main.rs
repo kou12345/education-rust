1b22f62eded28b6259f7da013064369afefe0d68
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-30 17:27:21 +0900

    handle malformed input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -24,7 +24,10 @@ fn main() {
             .expect("Failed to read line"); // エラーメッセージ
 
         // Stringインスタンスのtrimメソッドは文字列の先頭と末尾の空白をすべて削除する
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 

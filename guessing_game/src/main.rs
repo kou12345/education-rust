32649f19dfe9fdab13193d22185766d1384b2d5f
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-30 16:38:31 +0900

    WIP

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // mut 可変
+    // String型の新しいインスタンスを生成
+    let mut guess = String::new();
+
+    io::stdin()
+        // &mut guess は guessの参照
+        // 入力内容をguessに格納するため、mutである必要がある
+        .read_line(&mut guess)
+        .expect("Failed to read line"); // エラーメッセージ
+
+    println!("You guessed: {}", guess);
 }

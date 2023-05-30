581e2587f704e249971f9a9121714339f2906e24
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-30 17:09:46 +0900

    Changed the game to loop and Game ends when you win

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, cmp::Ordering};
+use std::{cmp::Ordering, io};
 
 use rand::Rng;
 
@@ -10,28 +10,32 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // mut 可変
-    // String型の新しいインスタンスを生成
-    let mut guess: String = String::new();
+        // mut 可変
+        // String型の新しいインスタンスを生成
+        let mut guess: String = String::new();
 
-    io::stdin()
-        // &mut guess は guessの参照
-        // 入力内容をguessに格納するため、mutである必要がある
-        .read_line(&mut guess)
-        .expect("Failed to read line"); // エラーメッセージ
+        io::stdin()
+            // &mut guess は guessの参照
+            // 入力内容をguessに格納するため、mutである必要がある
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // エラーメッセージ
 
-    // Stringインスタンスのtrimメソッドは文字列の先頭と末尾の空白をすべて削除する
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        // Stringインスタンスのtrimメソッドは文字列の先頭と末尾の空白をすべて削除する
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    // 予想と秘密の数を比較
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // 予想と秘密の数を比較
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }

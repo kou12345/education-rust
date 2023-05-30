4d5b65e65342e5ff87bd69ecfb21ed625a68a1f9
kou12345 <hi.0412.ko.2002@gmail.com>
2023-05-30 16:36:19 +0900

    add .gitignore

--- guessing_game/target/debug/deps/guessing_game-ed38a6488522a286.d
+++
@@ -1,5 +0,0 @@
-/home/kou12345/workspace/education-rust/guessing_game/target/debug/deps/guessing_game-ed38a6488522a286.rmeta: src/main.rs
-
-/home/kou12345/workspace/education-rust/guessing_game/target/debug/deps/guessing_game-ed38a6488522a286.d: src/main.rs
-
-src/main.rs:

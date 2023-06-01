c9527513551e1c10e26a32b4224409b3a4792a1a
kou12345 <hi.0412.ko.2002@gmail.com>
2023-06-01 14:00:24 +0900

    四則演算

---
+++ data_type/src/numerical_operation.rs
@@ -0,0 +1,28 @@
+pub fn numerical_operation() {
+    // addition
+    // 足し算
+    let sum = 5 + 10;
+    println!("sum: {}", sum);
+
+    // subtraction
+    // 引き算
+    let difference = 95.5 - 4.3;
+    println!("difference: {}", difference);
+
+    // multiplication
+    // 掛け算
+    let product = 4 * 30;
+    println!("product: {}", product);
+
+    // division
+    // 割り算
+    let quotient = 56.7 / 32.2;
+    println!("quotient: {}", quotient);
+    let floored = 2 / 3; // Results in 0
+    println!("floored: {}", floored);
+
+    // remainder
+    // 余り
+    let remainder = 43 % 5;
+    println!("remainder: {}", remainder);
+}
